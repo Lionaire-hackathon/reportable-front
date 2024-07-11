077611f9b150f5eb0cfa4b5d7b3bd5d886c24769
tachyon6 <songjunjun62754@gmail.com>
2024-07-12 00:36:45 +0900

    feat: 컴포넌트 구조 변경

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import HomePage from "./routes/HomePage";
 import EssayPage from "./routes/EssayPage";
 import SignUpPage from "./routes/SignUpPage";
