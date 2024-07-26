12aff7ea93ed5e2d7717711a1c97517fdc9d8bfc
Park <gse1818@naver.com>
2024-07-27 08:02:39 +0900

    feat : s3 파일업로들르 백엔드에서 하도록 수정

--- src/apis/file.jsx
+++ src/apis/file.jsx
@@ -1,8 +1,27 @@
 import axiosClient from "./axiosClient";
 import axios from "axios";
 
-const SERVER_API_URL = process.env.REACT_APP_ENV === "production" ? process.env.REACT_APP_PROD_SERVER_URI : process.env.REACT_APP_DEV_SERVER_URI;
+const SERVER_API_URL =
+    process.env.REACT_APP_ENV === "production"
+        ? process.env.REACT_APP_PROD_SERVER_URI
+        : process.env.REACT_APP_DEV_SERVER_URI;
 
 axios.defaults.baseURL = SERVER_API_URL;
 
-export const fileApi = async (fileDto) => axiosClient.post("/file", fileDto);
+export const fileApi = {
+    upload(file, createFileDto) {
+        const formData = new FormData();
+        formData.append("file", file);
+
+        // Append DTO fields to formData
+        Object.keys(createFileDto).forEach((key) => {
+            formData.append(key, createFileDto[key]);
+        });
+
+        return axiosClient.post("/file/upload", formData, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+        });
+    },
+};
